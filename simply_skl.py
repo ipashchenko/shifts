309b46b87777a1a7270c4e018efe0f5148ef3995
Ilya Pashchenko <in4pashchenko@gmail.com>
2017-08-08 06:18:39 +0300

    calculating corr. coefficients

--- simply_skl.py
+++ simply_skl.py
@@ -2,20 +2,20 @@ import os
 import glob
 import numpy as np
 import pandas as pd
-from scipy.stats import pearsonr
 from sklearn import preprocessing
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
-from utils import cv_ridge_sklearn
+from utils import cv_ridge_sklearn, OrderedDefaultDict
 
 
-# FIXME: Use max_p high but find the lowest power which has CV within sigma of
-# the best CV
 max_p = 3
+
+# This will be the Fomalont error where it is specified as ``0``
+zero_error_value = 0.01
 
 # This means choose ``k`` adaptively - not large then 10 but not smaller then 4
 k = None
-# Find all sources
+
 data_dir = '/home/ilya/github/shifts/data/errors'
 save_dir = '/home/ilya/github/shifts/data/errors'
 files = glob.glob(os.path.join(data_dir, "*kinem.txt"))
@@ -24,8 +24,10 @@ for fn in files:
     _, fn_ = os.path.split(fn)
     sources.append(fn_.split('.')[0])
 sources = list(set(sources))
-sources = ['0300+470']
+sources = ['0300+470', '2209+236']
 # sources = ['2209+236']
+corr_coefs = OrderedDefaultDict()
+corr_coefs_w = OrderedDefaultDict()
 
 for source in sources:
     print source
@@ -56,25 +58,17 @@ for source in sources:
     axes.legend(loc="upper left")
     axes.set_title(source)
     fig.tight_layout()
-    # fig.show()
-    # fig.savefig(os.path.join(save_dir, '{}_raw.png'.format(source)), dpi=300)
     canvas.print_figure(os.path.join(save_dir, '{}_raw.png'.format(source)), dpi=300)
-    # plt.close(fig)
-    #
     data_set = sorted(data_set, key=lambda df: np.min(df['time'].values))
 
-    # This for plotting average deviations
-    # fig, axes = plt.subplots(1, 1)
-    # This for plotting data with best fitted polynomials
     fig = Figure()
     axes = fig.add_subplot(1, 1, 1)
     canvas = FigureCanvas(fig)
 
     for i, df in enumerate(data_set):
-        df.loc[df['error'] == 0, 'error'] = 0.01
+        df.loc[df['error'] == 0, 'error'] = zero_error_value
         mm_scaler_t = preprocessing.MinMaxScaler()
         mm_scaler_y = preprocessing.MinMaxScaler()
-        # TODO: Here optionally choose the order of polynom
         tt = np.linspace(df['time'].values[0], df['time'].values[-1], 300)
         trend, trend_tt = cv_ridge_sklearn(df['time'].values,
                                            df['position'].values,
@@ -85,21 +79,23 @@ for source in sources:
         axes.plot(df['time'], df['position'], '.', label="")
         df['position'] -= trend
         data_set[i] = df
-        # axes.plot(df['time'], df['position'])
-        # axes.plot(df['time'], df['position'], '.')
     axes.legend(loc='upper left')
-    canvas.print_figure(os.path.join(save_dir, '{}_raw_fitted_ridge.png'.format(source)), dpi=300)
+    canvas.print_figure(os.path.join(save_dir,
+                                     '{}_raw_fitted_ridge.png'.format(source)),
+                        dpi=300)
 
     # Average residuals from fit
     try:
         data = data_set[0]
     except IndexError:
+        # Some sources have no components with number of epochs > then minimal
+        # required
         continue
+
     for i, df in enumerate(data_set[1:]):
         data = data.join(df.set_index('time'), how='outer', on='time',
                          lsuffix='_{}'.format(i+1), rsuffix='_{}'.format(i+2))
     data = data.sort_values('time')
-    # adata = np.array(data)
 
     # Plot averaged detrended positions vs Core Flux
     fig = Figure()
@@ -107,8 +103,6 @@ for source in sources:
     canvas = FigureCanvas(fig)
 
     minmax_scaler_shift = preprocessing.MinMaxScaler()
-    # FIXME: Use weights in averaging
-    # mean_deviations = np.nanmean(adata[:, 1:], axis=1)
     n_positions = len(data.columns) // 2
     positions = [data[data.columns[2 * i + 1]] for i in range(n_positions)]
     positions = np.vstack(positions).T
@@ -118,21 +112,47 @@ for source in sources:
     errors = np.ma.masked_array(errors, np.isnan(errors))
     mean_deviations_w = np.ma.average(positions, axis=1, weights=1./errors)
     mean_deviations = np.ma.average(positions, axis=1)
-
-    # mean_deviations = np.average(adata[:, 1:], axis=1)
-    axes.plot(data['time'], minmax_scaler_shift.fit_transform(mean_deviations), '.g')
-    axes.plot(data['time'], minmax_scaler_shift.transform(mean_deviations), 'g')
-    axes.plot(data['time'], minmax_scaler_shift.transform(mean_deviations_w), '.b')
-    axes.plot(data['time'], minmax_scaler_shift.transform(mean_deviations_w),
-              'b', label="weighted ave")
+    data['mean_deviations'] = mean_deviations
+    data['mean_deviations_w'] = mean_deviations_w
 
     flux = pd.read_table(os.path.join(data_dir,
                                       '{}.u1.core_flux.txt'.format(source)),
                          delim_whitespace=True, names=["time", "flux"],
                          engine='python', usecols=[0, 1])
-    # idx = np.where(flux[:, 0] > data['time'][0])
-    # axes.plot(flux[:, 0][idx], preprocessing.minmax_scale(flux[:, 1][idx]), 'r')
-    # axes.plot(flux[:, 0][idx], preprocessing.minmax_scale(flux[:, 1][idx]), '.r')
+
+    # Find common times in averaged residuals and flux DataFrames
+    data_flux = data.join(flux.set_index('time'), how='left', on='time')
+    # Find corr. coef between residuals and flux
+    corr_pearson = data_flux.corr(method='pearson')
+    corr_kendall = data_flux.corr(method='kendall')
+    corr_spearman = data_flux.corr(method='spearman')
+    corr_coefs[source] = {'pearson': corr_pearson['flux']['mean_deviations'],
+                          'kendall': corr_kendall['flux']['mean_deviations'],
+                          'spearman': corr_spearman['flux']['mean_deviations']}
+    corr_coefs_w[source] = {'pearson': corr_pearson['flux']['mean_deviations_w'],
+                            'kendall': corr_kendall['flux']['mean_deviations_w'],
+                            'spearman': corr_spearman['flux']['mean_deviations_w']}
+
+    # Labels for plots showing values of corr. coefficients
+    label_w = r"w ave $r_{{pear}}$={:.2f}, $r_{{kend}}$={:.2f}," \
+              r" $r_{{spear}}$={:.2f}".format(corr_pearson['flux']['mean_deviations_w'],
+                                              corr_kendall['flux']['mean_deviations_w'],
+                                              corr_spearman['flux']['mean_deviations_w'])
+    label = r"$r_{{pear}}$={:.2f}, $r_{{kend}}$={:.2f}," \
+            r"$r_{{spear}}$={:.2f}".format(corr_pearson['flux']['mean_deviations'],
+                                           corr_kendall['flux']['mean_deviations'],
+                                           corr_spearman['flux']['mean_deviations'])
+    axes.plot(data['time'],
+              minmax_scaler_shift.fit_transform(mean_deviations), '.g')
+    axes.plot(data['time'],
+              minmax_scaler_shift.transform(mean_deviations), 'g',
+              label=label)
+    axes.plot(data['time'],
+              minmax_scaler_shift.transform(mean_deviations_w), '.b')
+    axes.plot(data['time'],
+              minmax_scaler_shift.transform(mean_deviations_w), 'b',
+              label=label_w)
+
     axes2 = axes.twinx()
     minmax_scaler_flux = preprocessing.MinMaxScaler()
     axes2.plot(flux["time"], minmax_scaler_flux.fit_transform(flux["flux"]),
@@ -153,19 +173,8 @@ for source in sources:
 
     axes.set_ylabel("Averaged deviations from trend, mas", color='g')
     axes.set_title(source)
-    axes.legend(loc="best")
+    axes.legend(loc='best')
     fig.tight_layout()
-    canvas.print_figure(os.path.join(save_dir, '{}_average.png'.format(source)), dpi=300)
+    canvas.print_figure(os.path.join(save_dir, '{}_average.png'.format(source)),
+                        dpi=300)
 
-
-    # Fit shifts
-    # x_fine = np.linspace(adata[0, 0], adata[-1, 0], 1000)-adata[0, 0]
-    # results = optimize_gp(adata[:, 0]-adata[0, 0], adata[:, 1],
-    #                       p0=[0.2, 0.001, 1, 0.1])
-    # mu, cov = partial_predict(results['k1'], results['k'], adata[:, 0]-adata[0, 0], adata[:, 1],
-    #                           x_fine)
-    # std = np.sqrt(np.diag(cov))
-    # axes.fill_between(x_fine+adata[0, 0], mu-std, mu+std, color='g', alpha=0.25)
-    # fig.show()
-    # t_max = np.max([data[-1, 0] for data in data_set])
-    # t_fine = np.linspace(0, t_max, 500)
